06f52ea87ca0dc734ab67ecc1a7d2e41e3c67921
adeenayub <12beseaayub@seecs.edu.pk>
2018-11-06 13:15:33 +0500

    Create linkedin.py
    Linkedin password cracking for lowercase and already cracked passwords

---
+++ Assignment 1.2/linkedin.py
@@ -0,0 +1,42 @@
+import hashlib 
+import sys
+  
+dictfile = open("rockyou_utf8.txt", "r")
+pswrdfile = open("SHA1.txt", "r")
+rsltfile = open("linkycrackedpasswords.txt", "w+")
+
+passwrds = set()
+for line in pswrdfile:
+  passwrds.add(line.replace("\n","")) 
+
+#counter is maintained to ensure not more than 1K cracked passwords are written to the file(as said in the instructions)
+counter = 0
+for line in dictfile:
+	#print(line)
+	#str = line.replace("\n","").upper()
+	str2 = line.replace("\n","")
+	#result = hashlib.sha1(str.encode())
+	result2 = hashlib.sha1(str.encode())
+	crackedhash = result2.hexdigest()
+	#the leaked file has 00000 prepended to the hashes that have probably been already cracked by hackers
+	newhash = '0'*5 + crackedhash[5:]
+
+	#if result.hexdigest() in passwrds:
+	#	counter = counter+1
+		#only write to the file if less than 1K passwords are cracked and written
+	#	if counter <= 10:  
+	#		rsltfile.write(result.hexdigest()+" "+ str+"\n")
+	#		print(result.hexdigest()," ", str," ",counter)
+		
+	if newhash in passwrds:
+		counter = counter +1
+		if counter <= 1000:
+			rsltfile.write(newhash + " " + str2 + "\n")
+			print(newhash, " " , str2, " ", counter)
+	
+		else:
+			rsltfile.close()
+			dictfile.close()
+			pswrdfile.close()
+			print("1K passwords cracked Alhamdulillah. Program exiting")
+			break
